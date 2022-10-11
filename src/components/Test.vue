00949e1f7ceff998e56ab12766559472d94e6292
larrydu <larrydu@msi.com>
2022-10-11 18:18:17 +0800

    update Test component

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @click="$emit('check')">AAAAAAAAAAAAA</div>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+};
+</script>
+<script setup>
+import { onMounted, useAttrs } from "vue";
+
+const props = defineProps({
+  onCheck: {
+    type: Function,
+  },
+});
+defineEmits(["check"]);
+
+onMounted(() => {
+  const attrs = useAttrs();
+  console.log(props);
+});
+</script>
